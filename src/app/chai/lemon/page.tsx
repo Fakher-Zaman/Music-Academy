f2603ad51c56c5d068a11019a084f2bcf2df7de0
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-06 17:05:59 +0500

    Solving Navbar Challenge

--- src/app/chai/lemon/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LemonPage = () => {
-    return (
-        <h1 className='text-3xl text-center mt-8'>Lemon Page</h1>
-    )
-}
-
-export default LemonPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h2 className="text-2xl text-center mt-8">Start Music Academy Project</h2>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h2 className="text-2xl text-center mt-8">Start Music Academy Project</h2>
+    </main>
   );
 }
