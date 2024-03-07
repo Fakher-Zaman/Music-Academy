542f236e5d89784308c0ee4033f9316213a82582
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-07 10:51:04 +0500

    Solving Hero Section Challenge (Part 2)

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,7 +8,7 @@ function HeroSection() {
             className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
         >
             <Spotlight
-                className="-top-40 left-0 md:left-60 md:-top-20"
+                className="top-40 left-0 md:left-60 md:-top-20"
                 fill="white"
             />
             <div className="p-4 relative z-10 w-full text-center" >
