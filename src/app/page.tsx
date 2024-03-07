dc83d4e339ba17d494bbb7b51cd764f7d5cd8042
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-07 11:16:33 +0500

    Building Featured Card Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
