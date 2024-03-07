c1293d17b662ccff9530db2dac17c40466dabc05
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-03-07 10:29:53 +0500

    Solving Hero Section Challenge (Part 1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h2 className="text-2xl text-center mt-8">Start Music Academy Project</h2>
+      <HeroSection />
     </main>
   );
 }
